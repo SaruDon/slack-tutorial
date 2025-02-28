06e78ac4e96560776e9f5a0714b64546dc5d15c5
Sarvesh Khamkar <sarveshkhamkar321@gmail.com>
2025-02-28 10:24:38 +0530

    Join  new workspace

--- convex/workspace.ts
+++ convex/workspace.ts
@@ -12,6 +12,64 @@ const generateCode = () => {
   return code;
 };
 
+export const getInfoById = query({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx)
+    if (!userId) {
+      throw new Error("Unauthorized")
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.id).eq("userId", userId))
+      .unique()
+    const workspace = await ctx.db.get(args.id)
+    return { name: workspace?.name, isMember: !!member };
+  }
+})
+
+
+export const join = mutation({
+  args: {
+    joinCode: v.string(),
+    workspaceId: v.id("workspaces")
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx)
+    if (!userId) {
+      throw new Error("Unauthorized")
+    }
+
+    const workspace = await ctx.db.get(args.workspaceId)
+
+    if (!workspace) {
+      throw new Error("Workspace not found")
+    }
+
+    if (workspace.joinCode !== args.joinCode.toLocaleLowerCase()) {
+      throw new Error("Join code is incorrect")
+    }
+
+    const existingMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.workspaceId).eq("userId", userId))
+      .unique()
+
+    if (existingMember) {
+      throw new Error("Already part of workspace")
+    }
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId: workspace._id,
+      role: "member"
+    })
+
+    return workspace._id
+  }
+})
+
 export const newJoinCode = mutation({
   args: { workspaceId: v.id("workspaces") },
   handler: async (ctx, args) => {
---
+++ src/features/workspaces/api/use-get-workspace-info.ts
@@ -0,0 +1,17 @@
+import { getInfoById } from './../../../../convex/workspace';
+import { useQuery } from "convex/react"
+import { api } from "../../../../convex/_generated/api"
+import { Id } from "../../../../convex/_generated/dataModel"
+
+interface UserGetWorkspaceInfoProps {
+  id: Id<"workspaces">
+}
+
+
+export const useGetWorkSpaceInfo = ({ id }: UserGetWorkspaceInfoProps) => {
+  const data = useQuery(api.workspace.getInfoById, { id })
+  const isLoading = data === undefined
+
+
+  return ({ data, isLoading })
+}
